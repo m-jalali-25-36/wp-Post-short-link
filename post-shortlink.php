0905eaf1d8818ee390fb1e7442bb60c6819f3713
m-jalali-25-36 <m.jalali.25.36@gmail.com>
2022-02-10 23:29:06 +0330

    firste code

--- post-shortlink.php
+++ post-shortlink.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * Plugin Name: Post short link
@@ -7,3 +7,39 @@
  * Author: m-jalali
  * Author URI: http://www.m-jalali.ir
  */
+
+
+
+// Adding a new rule
+/**
+ * Adds a new rewrite rule.
+ *
+ * @param array $rules Existing rewrite rules.
+ * @return array (Maybe) modified list of rewrites.
+ */
+function wpdocs_insert_rewrite_rules($rules)
+{
+    $newrules = array();
+    $newrules['q/([^/]+)/?$'] = 'index.php?shortlink=$matches[1]';
+    return $newrules + $rules;
+}
+add_filter('rewrite_rules_array', 'wpdocs_insert_rewrite_rules');
+
+// Adding the id var so that WP recognizes it
+function wpdocs_insert_query_vars($vars)
+{
+    array_push($vars, 'shortlink');
+    return $vars;
+}
+function wpdocs_re_query_vars($vars)
+{
+    if (key_exists('shortlink', $vars)) {
+        $url = get_permalink($vars['shortlink']);
+        if (wp_redirect($url)) {
+            exit;
+        }
+    }
+    return $vars;
+}
+add_filter('query_vars', 'wpdocs_insert_query_vars');
+add_filter('request', 'wpdocs_re_query_vars');
