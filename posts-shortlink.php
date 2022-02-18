805b4f4a20093694687126c128a37259d3f54243
m-jalali-25-36 <m.jalali.25.36@gmail.com>
2022-02-18 22:14:24 +0330

    Version 1.2.0 Number of characters was added to the settings and comment functions and a readme .txt file was added and the plugin name was changed

--- posts-shortlink.php
+++ posts-shortlink.php
@@ -1,16 +1,59 @@
 <?php
 
 /**
- * Plugin Name: Post short link
- * Description: Post short link
- * Version: 1.1.0
- * Author: m-jalali
- * Author URI: http://www.m-jalali.ir
+ * Plugin Name: Posts short link
+ * Description: Posts short link
+ * Version: 1.2.0
+ * Author: jalali2536
+ * Author URI: https://profiles.wordpress.org/jalali2536/
  */
 
+/**
+ * For plugin 'page short link'.
+ * const query vars name.
+ */
 const psl_query_vars_name = 'shortlink';
+/**
+ * For plugin 'page short link'.
+ * const options name.
+ */
 const psl_option_name = 'psl_option';
 
+/**
+ * For plugin 'page short link'.
+ * @return array Restores the plugin default option.
+ */
+function psl_get_default_option()
+{
+    return array(
+        'slug' => 'q',
+        'indicator_mode' => 'decimal',
+        'character' => 0,
+    );
+}
+
+/**
+ * For plugin 'page short link'.
+ * @return array Restores the plugin option.
+ */
+function psl_get_option()
+{
+    $geted  = get_option(psl_option_name, array());
+    $def = psl_get_default_option();
+    foreach ($def as $key => $value) {
+        if (isset($geted[$key]))
+            $def[$key] = $geted[$key];
+    }
+    return $def;
+}
+
+/**
+ * For plugin 'page short link'.
+ * Convert a number from base arbitrary to ten base.
+ * @param string $key A base number of $arr.
+ * @param array $arr An array for the base.
+ * @return int The base number is ten
+ */
 function psl_convert_to_decimal($key, $arr)
 {
     $res = 0;
@@ -25,7 +68,16 @@ function psl_convert_to_decimal($key, $arr)
     }
     return $res;
 }
-function psl_convert_decimal_to($n, $arr)
+
+/**
+ * For plugin 'page short link'.
+ * Convert a number from base ten to arbitrary base.
+ * @param int $n A base ten number to convert.
+ * @param array $arr An array for the base.
+ * @param int $c Optional. Number of output characters.
+ * @return string New base number.
+ */
+function psl_convert_decimal_to($n, $arr, $c = 0)
 {
     $res = '';
     $n = (int)$n;
@@ -35,41 +87,77 @@ function psl_convert_decimal_to($n, $arr)
         $res = $arr[$b] . $res;
         $n = ($n - $b) / $size;
     }
+    $c -= strlen($res);
+    while ($c > 0) {
+        $res = $arr[0] . $res;
+        $c--;
+    }
     return $res;
 }
+/**
+ * For plugin 'page short link'.
+ * (A-Z) + (a-z) => 52
+ * @return array An array of fifty-two distinct characters.
+ */
 function psl_get_fiftytwo()
 {
     return  array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
 }
+
+/**
+ * For plugin 'page short link'.
+ * (0-9) + (a-z) => 36
+ * @return array An array of thirty-six distinct characters.
+ */
 function psl_get_thirtysix()
 {
     return array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
 }
+
+/**
+ * For plugin 'page short link'.
+ * (a-z) => 26
+ * @return array An array of twenty-six distinct characters.
+ */
 function psl_get_twentysix()
 {
     return  array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
 }
+
+/**
+ * For plugin 'page short link'.
+ * (0-9) + (a-f) => 16
+ * @return array An array of sixteen distinct characters
+ */
 function psl_get_hexadecimal()
 {
     return  array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f');
 }
 
+/**
+ * For plugin 'page short link'.
+ * Get indicator by id.
+ * @param int $id A number as id.
+ * @return string A string as an indicator.
+ */
 function psl_get_indicator($id)
 {
-    $option = get_option(psl_option_name, array());
+    $option = psl_get_option();
     if (isset($option['indicator_mode'])) {
+        if (!isset($option['character']))
+            $option['character'] = 0;
         switch ($option['indicator_mode']) {
             case 'hexadecimal':
-                return psl_convert_decimal_to($id, psl_get_hexadecimal());
+                return psl_convert_decimal_to($id, psl_get_hexadecimal(), $option['character']);
 
             case 'twentysix':
-                return psl_convert_decimal_to($id, psl_get_twentysix());
+                return psl_convert_decimal_to($id, psl_get_twentysix(), $option['character']);
 
             case 'thirtysix':
-                return psl_convert_decimal_to($id, psl_get_thirtysix());
+                return psl_convert_decimal_to($id, psl_get_thirtysix(), $option['character']);
 
             case 'fiftytwo':
-                return psl_convert_decimal_to($id, psl_get_fiftytwo());
+                return psl_convert_decimal_to($id, psl_get_fiftytwo(), $option['character']);
 
             case 'decimal':
             default:
@@ -79,30 +167,54 @@ function psl_get_indicator($id)
     return $id;
 }
 
-function get_shortlink($id)
-{
-    return get_home_url() . '/' . psl_get_slug()  . '/' . psl_get_indicator($id);
+if (!function_exists('get_shortlink')) {
+    /**
+     * For plugin 'page short link'.
+     * Get short link by id.
+     * @param int $id A number as id.
+     * @return string short link.
+     */
+    function get_shortlink($id)
+    {
+        return get_home_url() . '/' . psl_get_slug()  . '/' . psl_get_indicator($id);
+    }
 }
 
+/**
+ * For plugin 'page short link'.
+ * Add a column title to the posts table.
+ * Filter 'manage_posts_columns'
+ */
 function psl_add_column_get_shortlink_title($columns)
 {
     return array_merge($columns, array('shortlink' => __('Short Link')));
 }
+add_filter('manage_posts_columns', 'psl_add_column_get_shortlink_title', 1);
+
+/**
+ * For plugin 'page short link'.
+ * Add short link column content to the posts table
+ * Action 'manage_posts_custom_column'
+ */
 function psl_add_column_get_shortlink($column, $id)
 {
     if ($column == 'shortlink') {
         $indicator = psl_get_indicator((int)$id);
         $slug = psl_get_slug();
         $url_shortlink = get_home_url() . '/' . $slug . '/' . $indicator;
-        echo '<p onclick="navigator.clipboard.writeText(\'' . $url_shortlink . '\');alert(\'Copied the link\')">' . $slug . '/' . $indicator . '</p>';
+        echo '<p onclick="navigator.clipboard.writeText(\'' . $url_shortlink . '\');alert(\'Copied the link\')" title="Click to copy the address. ' . $url_shortlink . '">' . $slug . '/' . $indicator . '</p>';
     }
 }
-add_filter('manage_posts_columns', 'psl_add_column_get_shortlink_title', 1);
 add_action('manage_posts_custom_column', 'psl_add_column_get_shortlink', 2, 2);
-// manage_book_posts_columns manage_posts_custom_column
+
+/**
+ * For plugin 'page short link'.
+ * Returns the slug.
+ * @return string slug.
+ */
 function psl_get_slug()
 {
-    $option = get_option(psl_option_name, array());
+    $option = psl_get_option();
     if (isset($option['slug']))
         return $option['slug'];
     else
@@ -110,7 +222,9 @@ function psl_get_slug()
 }
 
 /**
- * Flushes rewrites if our project rule isn't yet added.
+ * For plugin 'page short link'.
+ * Flushes rewrites if our short link rule isn't yet added.
+ * Action 'wp_loaded'
  */
 function psl_flush_rules()
 {
@@ -123,10 +237,10 @@ function psl_flush_rules()
 }
 add_action('wp_loaded', 'psl_flush_rules');
 
-// Adding a new rule
 /**
- * Adds a new rewrite rule.
- *
+ * For plugin 'page short link'.
+ * Adds a short link rewrite rule.
+ * Filter 'rewrite_rules_array'
  * @param array $rules Existing rewrite rules.
  * @return array (Maybe) modified list of rewrites.
  */
@@ -139,7 +253,13 @@ function psl_insert_rewrite_rules($rules)
 }
 add_filter('rewrite_rules_array', 'psl_insert_rewrite_rules');
 
-// Adding the id var so that WP recognizes it
+/**
+ * For plugin 'page short link'.
+ * Adding the shortlink var so that WP recognizes it.
+ * Filter 'query_vars'
+ * @param array $vars
+ * @return array
+ */
 function psl_insert_query_vars($vars)
 {
     array_push($vars, psl_query_vars_name);
@@ -147,12 +267,18 @@ function psl_insert_query_vars($vars)
 }
 add_filter('query_vars', 'psl_insert_query_vars');
 
-
+/**
+ * For plugin 'page short link'.
+ * Checks if there is a short link redirects to the main address otherwise continues.
+ * Filter 'request'
+ * @param array $vars
+ * @return array
+ */
 function psl_redirect($vars)
 {
     if (key_exists(psl_query_vars_name, $vars)) {
         $id = -1;
-        $option = get_option(psl_option_name, array());
+        $option = psl_get_option();
         if (isset($option['indicator_mode'])) {
             switch ($option['indicator_mode']) {
                 case 'hexadecimal':
@@ -196,24 +322,24 @@ add_filter('request', 'psl_redirect');
 
 
 /**
- * 
- * Panel Admin
- * 
+ * For plugin 'page short link'.
+ * Add Panel options for Admin.
+ * Filter 'admin_menu'
  */
-
 function psl_add_menu()
 {
-    $tt_page = add_submenu_page("options-general.php", "Post short link", "Post short link", "manage_options", "psl-panel", "psl_admin_panel_display", null, 99);
+    $tt_page = add_submenu_page("options-general.php", "Posts short link", "Posts short link", "manage_options", "psl-panel", "psl_admin_panel_display", null, 99);
 }
 add_action("admin_menu", "psl_add_menu");
 
+/**
+ * For plugin 'page short link'.
+ * Admin panel viewer.
+ */
 function psl_admin_panel_display()
 {
     if (isset($_POST['submit'])) {
-        $def = array(
-            'slug' => 'q',
-            'indicator_mode' => 'decimal',
-        );
+        $def = psl_get_default_option();
         foreach ($def as $key => $value) {
             if (isset($_POST[$key]))
                 $def[$key] = $_POST[$key];
@@ -221,14 +347,11 @@ function psl_admin_panel_display()
         update_option(psl_option_name, $def);
     }
 
-    $option = get_option(psl_option_name, array(
-        'slug' => 'q',
-        'indicator_mode' => 'decimal',
-    ));
+    $option = psl_get_option();
     $indicator_mode = array('decimal', 'hexadecimal', 'twentysix', 'thirtysix', 'fiftytwo');
 ?>
     <div class="wrap">
-        <h1>Post short link</h1>
+        <h1>Posts short link</h1>
         <form action="" method="post">
             <table class="form-table" role="presentation">
                 <tr>
@@ -247,6 +370,10 @@ function psl_admin_panel_display()
                             ?>
                         </select>
                     </td>
+                </tr>
+                <tr>
+                    <th scope="row"><label for="character">Character</label></th>
+                    <td><input name="character" type="number" id="character" value="<?php echo $option['character']; ?>" class="regular-number" /></td>
                 </tr>
             </table>
             <?php submit_button(); ?>
---
+++ readme.txt
@@ -0,0 +1,16 @@
+=== Posts short link ===
+Contributors: jalali2536
+Tags: marketing-automation, shortlink, affiliate-marketing, affiliates, cloaker, social-media, marketing, rewrite, links, urls, shortlinks, link branding, link tracking, affiliate links, link cloaking, redirect, forward, shorturl, shorten, shortening, click, clicks, track, tracking, tiny, tinyurl, budurl, shrinking, domain, shrink, mask, masking, cloak, cloaking, slug, slugs, admin, administration, stats, statistics, stat, statistic, email, ajax, javascript, ui, csv, download, page, post, pages, posts, shortcode, seo, automation, widget, widgets, dashboard
+Tested up to: 5.8.3
+License: CC BY-NC-SA 4.0
+License URI: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+A plugin for creating short links to site content.
+
+== Description ==
+A plugin for creating short links to site content. For sharing on social networks and convenience in typing links.
+
+== Installation ==
+1. Upload \'Posts-shortlink.zip\' to the \'/wp-content/plugins/\' directory
+2. Activate the plugin through the \'Plugins\' menu in WordPress
+3. Adjust plugin settings.
